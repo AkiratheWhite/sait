02239e53f9c3476023d3c53add7cb0c9c7137266
OujiKarasuma <li.501stClone@gmail.com>
2020-11-19 08:52:53 -0700

    Removed event listeners from index.html and moved to register.hmtl. Made even listeners for buttons inline and moved functions to a new JS file.

---
+++ js/register-scripts.js
@@ -0,0 +1,19 @@
+function confirmSubmit() {
+	if (confirm("Submit the form?")) {
+		return true;
+	}
+
+	else {
+		return false;
+	}
+}
+
+function confirmReset() {
+	if (confirm("Reset the form?")) {
+		return true;
+	}
+
+	else {
+		return false;
+	}
+}
